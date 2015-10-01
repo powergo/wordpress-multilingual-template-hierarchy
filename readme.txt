df4e5f0d7c192ea84d0bba20671f3a5e72dd8d44
bartdag-resulto <bartdag-resulto@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-01 13:31:52 +0000

    initial import
    git-svn-id: https://plugins.svn.wordpress.org/multilingual-template-hierarchy/trunk@1257438 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== Multilingual Template Hierarchy ===
+Contributors: bartdag-resulto
+Tags: multilingual
+Requires at least: 4
+Tested up to: 4.3.1
+Stable tag: trunk
+License: GPL v3.0
+License URI: http://www.gnu.org/licenses/gpl-3.0.en.html
+
+Enables theme developers using WPML to create type-slug.php templates in the default language. For example, if you create page-my-english-slug.php template, it will be used for the same page in other languages.
+
+== Description ==
+Enables theme developers using WPML to create type-slug.php templates in the default language. 
+
+Currently, with WPML, if you want to create a custom template for a page/category/post, you need to create such template in each language (e.g., page-english-slug.php, page-french-slug.php). With this plugin, you can create the template only for the default language (e.g., page-english-slug.php) and it will be used for all other languages.
+
+Version 1.0.0 supports translated page, category and tag slugs.
+
+== Installation ==
+1. Upload the entire `multilingual-template-hierarchy` folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the \'Plugins\' menu in WordPress.
+
+
+== Changelog ==
+= 1.0.0 =
+
+* release date: October 1st 2015
+* Initial release
---
+++ wp-multilingual-template-hierarchy.php
@@ -0,0 +1,128 @@
+<?php
+/*
+Plugin Name: Multilingual Template Hierarchy
+Plugin URI:  https://github.com/resulto-admin/wordpress-multilingual-template-hierarchy
+Description: Enables theme developers using WPML to create type-slug.php templates in the default language. For example, if you create page-my-english-slug.php template, it will be used for the same page in other languages.
+Version:     1.0.0
+Author:      Resulto Developpement Web Inc.
+Author URI:  http://www.resulto.ca
+License:     GPL3
+License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+*/
+
+// TODO Refactor and extract common code :-)
+// TODO Add support for translated taxonomies.
+
+add_filter( 'page_template', 'multi_lingual_page_template');
+
+function multi_lingual_page_template($template) {
+    global $post;
+    $template_name = basename($template);
+    if (!($template_name === 'page.php' || $template_name === 'singular.php' || $template_name === 'index.php')) {
+        return $template;
+    }
+    $current_lang = apply_filters('wpml_current_language', NULL);
+    $default_lang = apply_filters('wpml_default_language', NULL);
+    if ($current_lang === $default_lang) {
+        return $template;
+    }
+
+    $id_in_default_lang = apply_filters('wpml_object_id', $post->ID, 'page', FALSE, $default_lang);
+
+    if (!$id_in_default_lang) {
+        return $template;
+    }
+
+    $post = get_post($id_in_default_lang);
+
+    if (!$post) {
+        return $template;
+    }
+
+    $template_name = 'page-' . $post->post_name . '.php';
+
+    $template_path = locate_template($template_name);
+
+    if ($template_path) {
+        return $template_path;
+    }
+
+    return $template;
+}
+
+
+add_filter( 'category_template', 'multi_lingual_category_template');
+
+function multi_lingual_category_template($template) {
+    $template_name = basename($template);
+    if (!($template_name === 'category.php' || $template_name === 'archive.php' || $template_name === 'index.php')) {
+        return $template;
+    }
+    $current_lang = apply_filters('wpml_current_language', NULL);
+    $default_lang = apply_filters('wpml_default_language', NULL);
+    if ($current_lang === $default_lang) {
+        return $template;
+    }
+
+    $category_id = get_query_var('cat');
+    $id_in_default_lang = apply_filters('wpml_object_id', $category, 'category', FALSE, $default_lang);
+
+    if (!$id_in_default_lang) {
+        return $template;
+    }
+
+    $category = get_category($id_in_default_lang);
+
+    if (!$category) {
+        return $template;
+    }
+
+    $template_name = 'category-' . $category->slug . '.php';
+
+    $template_path = locate_template($template_name);
+
+    if ($template_path) {
+        return $template_path;
+    }
+
+    return $template;
+}
+
+add_filter( 'tag_template', 'multi_lingual_tag_template');
+
+function multi_lingual_tag_template($template) {
+    $template_name = basename($template);
+    if (!($template_name === 'tag.php' || $template_name === 'archive.php' || $template_name === 'index.php')) {
+        return $template;
+    }
+    $current_lang = apply_filters('wpml_current_language', NULL);
+    $default_lang = apply_filters('wpml_default_language', NULL);
+    if ($current_lang === $default_lang) {
+        return $template;
+    }
+
+    $tag_id = get_query_var('tag');
+    $id_in_default_lang = apply_filters('wpml_object_id', $tag_id, 'tag', FALSE, $default_lang);
+
+    if (!$id_in_default_lang) {
+        return $template;
+    }
+
+    $tag = get_tag($id_in_default_lang);
+
+    if (!$tag) {
+        return $template;
+    }
+
+    $template_name = 'tag-' . $tag->slug . '.php';
+
+    $template_path = locate_template($template_name);
+
+    if ($template_path) {
+        return $template_path;
+    }
+
+    return $template;
+}
+
+?>
